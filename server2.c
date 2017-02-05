8232769dbddd36a7e68c5529c49a34bb5bd1f464
AcidDreamer <nmandrelas@gmail.com>
2017-02-05 14:05:21 +0200

    --Comments added,shutdown function added

--- server2.c
+++ server2.c
@@ -11,18 +11,19 @@
 #include <strings.h>
 #include <stdbool.h>
 
-#define MAX_CLIENTS 4
+#define MAX_CLIENTS 2
 #define DATA_LENGTH 2048
 #define ERROR -1
 
-//A checking mechanism for the empty server slots
+//A checking mechanism for the empty server slots --> flag[x]==0 then its empty or if flag[x] ==1 the spot is habitted
 static int flag[MAX_CLIENTS] ;
 //Where the client socket descriptors will be stored
 static int cl_sc[MAX_CLIENTS];
-//client structure for the  clients
+//client structure for the  clients of type sockaddr_in
 static struct sockaddr_in client;
 //socket descriptor for the Server
 static int server_sock_desc;
+//addrlen specifies the size, in bytes, of the address structure pointed to by addr
 static int sockaddr_len = sizeof (struct sockaddr_in);
 
 void Sent_to_self();
@@ -32,13 +33,10 @@ void *server_to_client(void *);
 void *server_accepting();
 
 int main(int argc, char** argv) {
-    //server structure
+    //server structure of type sockaddr_in
     struct sockaddr_in server;
-    //both are of type sockaddr_in
-
-    //socket descriptor for the first client
+    //socket descriptor for the client -->will be stored seperatly at cl_sc[]
     int client_sock_desc;
-    //socket descriptor for the second client
     //Number of bytes of a message
     int data_length;
     //The actual messege
@@ -55,12 +53,16 @@ int main(int argc, char** argv) {
     if ((server_sock_desc = socket(AF_INET, SOCK_STREAM, 0)) == ERROR) {
         perror("Server socket :");
         exit(-1);
-    };
-
+    }
+    //Indicates that the rules used in validating addresses supplied in a bind(2) call should allow reuse of local addresses
+    //For AF_INET sockets this means that a socket may bind, except when  there is an active listening socket bound to the address.
+    int enable = 1;
+    if (setsockopt(server_sock_desc, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
+        error("setsockopt(SO_REUSEADDR) failed");
+        
     //setting up the server data structure
-    server.sin_family = AF_INET;
-    //set-up the port via argument
-    server.sin_port = htons(atoi(argv[1]));
+    server.sin_family = AF_INET; //IPv4     
+    server.sin_port = htons(atoi(argv[1]));     //set-up the port via argument
     //contains the IP address that will be associated with the socket
     server.sin_addr.s_addr = INADDR_ANY; //kernel listens to all interfaces,0.0.0.0
     //initialize the data structure
@@ -78,7 +80,8 @@ int main(int argc, char** argv) {
         perror("Listen :");
         exit(-1);
     }
-    puts("Server is online");
+    
+    puts("Server is online,at any moment press //quit to terminate it");
     pthread_t sAThread;
     pthread_create(&sAThread, NULL, server_accepting,NULL);
     for(;;){
@@ -89,14 +92,20 @@ int main(int argc, char** argv) {
             int imaquiter;
             for(imaquiter =0 ;imaquiter<MAX_CLIENTS;imaquiter++){
                 if(flag[imaquiter]==1){
-                    puts("Client socket disconnected");
-                    close(cl_sc[imaquiter]);
+                    if(close(cl_sc[imaquiter])==0){
+                        puts("Client socket disconnected");
+                    }else{
+                        puts("Error terminating client connection");
+                    }
                 }
             }
-            puts("Server Socket Terminated");
-            close(server_sock_desc);
-            puts("Server closed gracefully");
-            pthread_kill(sAThread,2);
+            if(close(server_sock_desc)==0){
+                puts("Server Socket Terminated");
+            }else{
+                puts("Error terminating server socket");
+            }
+            sleep(10);  //waiting for any awk packets
+            puts("Server shut down gracefully");
             return 1;
         }
         
@@ -106,17 +115,22 @@ int main(int argc, char** argv) {
 }
 
 void *server_accepting(){
-    int client_sock_desc;
-    int secondFlag;
-    //if the user is first or the second user to connect
-    int whichOne[3];
-     //0 every value;
-    int counter;
+    /*
+    client_sock_desc --> socket descriptor provided after we accept a client
+    secondFlag --> flag to indicate if there are any available space for a new client
+    whichOne[0] --> contains the socket descriptor of the new client to pass to the server_to client thread
+    counter --> just a generic counter used for fors
+    */ 
+    int client_sock_desc, secondFlag, whichOne[3], counter;
+    //initializing flag[]
     for(counter = 0; counter<MAX_CLIENTS;counter++){
         flag[counter]=0;
     }
-    //Give a type of "id" to the user after accepting the connection
+    puts("Accepting Thread initialized");
+    
+    //Give a type of "id" to the user after accepting the connection and initialize his thread
     while ((client_sock_desc = accept(server_sock_desc, (struct sockaddr *) &client, (socklen_t*) & sockaddr_len))) {
+        //check if there is space for a new client 
         for(counter=0;counter<MAX_CLIENTS;counter++){
             if (flag[counter] == 0) {
                 //store the socket
@@ -131,43 +145,44 @@ void *server_accepting(){
                 break;
             } 
         }
+        //if there is not ,inform and close the clients socket
         if(secondFlag==0){
-                //send messege informing the user
                 char messege[] = "Connection limit exceeded ,try again later.\n";
                 send(client_sock_desc, messege, sizeof (messege), 0);
-                //close the connection
                 close(client_sock_desc);
-                //continue looping
                 continue;
+        //else initialize his thread
         }else{
-            //create thread and pass cl_sc as arguement
             pthread_t sTcThread;
             pthread_create(&sTcThread, NULL, server_to_client, (void*) whichOne);
         }
+        //reset the flag
         secondFlag=0;
     }
 }
-void *server_to_client(void *socket_desc) {
-    //make the arguement readable 
-    int *whichOneImported = (int *) socket_desc;
-    int whichOne = whichOneImported[0];
-    //one int for retrieved data and one for his socket
-    int retrieve, socket = cl_sc[whichOne];
 
+void *server_to_client(void *socket_desc) {
+    //type correct the argument thats passed
+    int *whichOneImported = (int *) socket_desc;
+    //get a local variable for the argument
+    int whichOne = whichOneImported[0];
+    //one int for retrieved data and one for the clients socket
+    int retrieve, socket = cl_sc[whichOne];
     //the actual data
     char data[DATA_LENGTH];
-    //free the string
-    memset(data, 0, DATA_LENGTH);
-    //if the user is not alone in the server
+    
+    memset(data, 0, DATA_LENGTH);       //free the string
+    
+    //if the user is not alone in the server,the moment he connects
     if (!Check_if_empty(whichOne)) {
         char messege[] = "Server ---> Another user entered the room.\n";
         Sent_to_all_others(whichOne, messege, sizeof (messege), 0);
     }
+    
+    //infinite loop
     for (;;) {
-        //we free the string in everyloop
-        memset(data, 0, DATA_LENGTH);
-        //we retrieve the data
-        retrieve = recvfrom(socket, data, DATA_LENGTH, 0, NULL, NULL);
+        memset(data, 0, DATA_LENGTH);         //we free the string in everyloop
+        retrieve = recvfrom(socket, data, DATA_LENGTH, 0, NULL, NULL);         //we retrieve the data
         //if the user is alone in the server
         if (Check_if_empty(whichOne)) {
             char messege[] = "Server ---> You are alone in the room.\n";
@@ -176,7 +191,7 @@ void *server_to_client(void *socket_desc) {
             //If an error occured
             if (retrieve < 0) {
                 printf("Error receiving data!\n");
-                //if we retrieved the data succesfully
+            //if we retrieved the data succesfully
             } else if (retrieve > 0) {
                 Sent_to_all_others(whichOne, data, DATA_LENGTH, 0);
                 printf("server: ");
